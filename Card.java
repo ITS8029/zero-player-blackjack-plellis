dacddd0d6776cb4f73f2d86184e9bf4787f6c8be
Paulo Lellis <paulolellis@Paulo-Lelliss-MacBook-Pro.local>
2017-10-20 13:44:38 -0400

    Coded a simple Blackjack game

--- Card.java
+++ Card.java
@@ -1,4 +1,3 @@
-package com.adampolt;
 
 public class Card {
 	/**
--- ZeroPlayerBlackjack.java
+++ ZeroPlayerBlackjack.java
@@ -1,10 +1,53 @@
-package com.adampolt;
+import java.util.Scanner;
+
 
 public class ZeroPlayerBlackjack {
 
+	/**
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to the casino!");		
+			
+		Deck myDeck = new Deck();
+		
+		int total = 0;
+		
+		while(total < 17) {
+		
+		Card myCard = myDeck.drawCard();
+		
+		int cardValue = myCard.getValue();		
+		
+		total = cardValue + total;
+		
+		String cardName = myCard.toString();
+			
+		System.out.println("You got a " + cardName);
+				
+		System.out.println("Your total is " + total);
+		
+		}
+		
+		//Check whether the player won or lost.
+		
+		if (total < 21) { 
+			System.out.println("You win!"); }
+		else if (total == 21) {
+			System.out.println("Blackjack!"); }
+		else if (total > 21) {
+			System.out.println("You lose!");
+			
+		}
 
-	}
+	}}
+	
 
-}
+			
+	
+	
+	
+
